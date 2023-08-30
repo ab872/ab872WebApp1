4b08ff0345b86c77f1bf9cb5c853f14624990526
A. Bright <ab8720843@gmail.com>
2023-08-30 13:30:58 -0700

    add Tools page add color table page under Tools

---
+++ Pages/mainTools.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webApp1.Pages_
+{
+    public class mainToolsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
